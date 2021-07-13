096cd649c5ab85442a7e820444d873f764dd8589
Jerry Henriquez <jerryhenriquez@gmail.com>
2021-07-13 16:21:39 +0200

    modified counter check value to 1621.13

--- scripts/simulateChange.ps1
+++ scripts/simulateChange.ps1
@@ -6,7 +6,9 @@ while ($true) {
     if ( (Get-Random -Minimum 1 -Maximum 10) -lt 5 ) {
         Get-Random | Set-Content -NoNewline -Path $filepath
         Start-Sleep -s 10
-        Start-Job -FilePath (Join-Path $PSScriptRoot "validateFileHash.ps1")
+        Write-Host "Ini Validate"
+        Start-Process -NoNewWindow Powershell (Join-Path $PSScriptRoot "validateFileHash.ps1") -Wait
+        Write-Host "End Validate"
     }
     Write-Host "Start Wait $(Get-Date)"
     Start-Sleep -s 240
